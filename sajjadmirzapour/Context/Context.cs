bbee3c40cfe33234207ea59be08cc6af5ea654e4
Sajad <Sajad@DESKTOP-V3FCRGT>
2022-05-10 12:46:50 +0430

    Add project files.

---
+++ sajjadmirzapour/Context/Context.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sajjadmirzapour.Models;
+
+namespace sajjadmirzapour
+{
+    public class Context : DbContext
+    {
+
+
+
+
+        public Context(DbContextOptions<Context> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<News> News { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+
+    }
+}
